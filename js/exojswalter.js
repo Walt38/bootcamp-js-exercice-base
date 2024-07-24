970ffeb1d19804775d0277c6e5a6fc8f252d76ac
Morell <walter.morell38@gmail.com>
2024-07-24 12:59:42 +0200

    ddd

--- js/exojswalter.js
+++ js/exojswalter.js
@@ -1,7 +1,9 @@
-/** Exo 1 Robot musée **/
+/** Exo 1 Robot musée /** */
 
+let userAge = parseInt(prompt("Quel est votre age ?")); //instancier un prompte pour entrer l'age
+const Price = calculatePrice(userAge);
 
-function Prix(Age) {
+function calculatePrice(Age) {
 
 //adulte(>=12)                  if
 //enfant(=>5 & <12)             else if
@@ -22,6 +24,24 @@ else{
     return ("le prix du billet est gratuit");
 }
 }
-console.log(Prix(20));
-console.log(Prix(11));
-console.log(Prix(4));
+//console.log(Price(20));
+//console.log(Price(11));
+//console.log(Price(4));
+
+console.log(calculatePrice(userAge)); //affichage console
+alert(calculatePrice(userAge)); // affichage du prix dans une alert
+
+
+
+
+
+/**Exo 2 : La moyenne de la classe */
+
+
+function moyenne(){
+
+let notes = []; //instancier la variable d' un tableau vide
+
+}
+//let notesInput = prompt("entrez votre note");
+
